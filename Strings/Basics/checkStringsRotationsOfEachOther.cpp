007f59f67bb2b9a06a9b9e7efefec3387c4c093a
Ananya Gupta <ananyagupta2306@gmail.com>
2021-02-22 01:34:19 +0530

    Check if gven strings are rotations of each other GFG

---
+++ Strings/Basics/checkStringsRotationsOfEachOther.cpp
@@ -0,0 +1,25 @@
+// GFG	
+// Check if two given strings are rotations of each other 
+
+# include <bits/stdc++.h> 
+using namespace std; 
+
+bool areRotations(string str1, string str2) 
+{ 
+if (str1.length() != str2.length()) 
+		return false; 
+
+string temp = str1 + str1; 
+return (temp.find(str2) != string::npos); 
+} 
+
+int main() 
+{ 
+string str1, str2;
+cin>>str1>>str2;
+if (areRotations(str1, str2)) 
+	printf("Strings are rotations of each other"); 
+else
+	printf("Strings are not rotations of each other"); 
+return 0; 
+} 
