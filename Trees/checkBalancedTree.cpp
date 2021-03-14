241159efe391a61444cdac591d0cd3d28e64f1a2
Ananya Gupta <ananyagupta2306@gmail.com>
2021-03-15 00:07:12 +0530

    Check if tree is height Balanced or not

---
+++ Trees/checkBalancedTree.cpp
@@ -0,0 +1,161 @@
+// Check for Balanced Tree 
+
+// Given a binary tree, find if it is height balanced or not. 
+// A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. 
+
+// A height balanced tree
+//         1
+//      /     \
+//    10      39
+//   /
+// 5
+
+// An unbalanced tree
+//         1
+//      /    
+//    10   
+//   /
+// 5
+
+// Example :
+
+// Input:
+//       1
+//     /
+//    2
+//     \
+//      3 
+// Output: 0
+// Explanation: The max difference in height
+// of left subtree and right subtree is 2,
+// which is greater than 1. Hence unbalanced
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+bool isBalanced(Node *root);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        cout << isBalanced(root) << endl;
+    }
+    return 1;
+}
+
+/* A binary tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return tree if passed  tree 
+// is balanced, else false. 
+
+int height(Node* node){
+    if(node==NULL) return 0;
+    return max(height(node->left),height(node->right))+1;
+}
+    
+bool isBalanced(Node *root)
+{
+    if(root==NULL) return true;
+    if(abs(height(root->left)-height(root->right))>1) return false;
+    else return isBalanced(root->left) && isBalanced(root->right);
+}
