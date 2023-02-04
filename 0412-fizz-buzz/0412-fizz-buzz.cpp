abbf087d0751493d8a91f2435b87d81c033c488d
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-02-04 09:11:32 +0530

    Time: 7 ms (34.99%), Space: 7.3 MB (89.80%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
+        // vector<string>v(n);
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 && i%5==0) v[i-1]="FizzBuzz";
+        //     else if(i%3==0) v[i-1]="Fizz";
+        //     else if(i%5==0) v[i-1]="Buzz";
+        //     else v[i-1]=to_string(i);
+        // }
+        // return v;
+        
+        // vector<string>v(n);
+        // for(int i=1;i<=n;i++){
+        //     int a=i%3, b=i%5;
+        //     if(a==0 && b==0) v[i-1]="FizzBuzz";
+        //     else if(a==0) v[i-1]="Fizz";
+        //     else if(b==0) v[i-1]="Buzz";
+        //     else v[i-1]=to_string(i);
+        // }
+        // return v;
+        
         vector<string>v(n);
+        int j=3,k=5;
         for(int i=1;i<=n;i++){
-            int a=i%3;
-            int b=i%5;
-            if(a==0 && b==0) v[i-1]="FizzBuzz";
-            else if(a==0) v[i-1]="Fizz";
-            else if(b==0) v[i-1]="Buzz";
+            if(i==j && i==k) {v[i-1]="FizzBuzz"; j+=3; k+=5;}
+            else if(i==j) {v[i-1]="Fizz";j+=3;}
+            else if(i==k) {v[i-1]="Buzz";k+=5;}
             else v[i-1]=to_string(i);
         }
         return v;
