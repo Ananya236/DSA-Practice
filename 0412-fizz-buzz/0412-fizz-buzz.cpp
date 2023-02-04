6c9dc2a0bd0d04dd4d540bb007ffa1925b2b99eb
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-02-04 09:18:30 +0530

    Time: 3 ms (81.69%), Space: 7.2 MB (89.80%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -20,13 +20,23 @@ public:
         // }
         // return v;
         
+        // vector<string>v(n);
+        // int j=3,k=5;
+        // for(int i=1;i<=n;i++){
+        //     if(i==j && i==k) {v[i-1]="FizzBuzz"; j+=3; k+=5;}
+        //     else if(i==j) {v[i-1]="Fizz";j+=3;}
+        //     else if(i==k) {v[i-1]="Buzz";k+=5;}
+        //     else v[i-1]=to_string(i);
+        // }
+        // return v;
+        
         vector<string>v(n);
-        int j=3,k=5;
+        int j=0,k=0;
         for(int i=1;i<=n;i++){
-            if(i==j && i==k) {v[i-1]="FizzBuzz"; j+=3; k+=5;}
-            else if(i==j) {v[i-1]="Fizz";j+=3;}
-            else if(i==k) {v[i-1]="Buzz";k+=5;}
-            else v[i-1]=to_string(i);
+            j++;k++;
+            if(j==3) {v[i-1]+="Fizz";j=0;}
+            if(k==5) {v[i-1]+="Buzz";k=0;}
+            if(v[i-1]=="") v[i-1]=to_string(i);
         }
         return v;
     }
