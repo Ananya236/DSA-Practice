a5b31c42f4fe6b08f5e8f2cd93eb46dcead8b8fd
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-05-17 07:56:39 +0530

    Time: 3 ms (57.15%), Space: 6 MB (35.98%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -4,7 +4,7 @@ public:
         int n = columnTitle.length();
         int res=0;
         for(int i=0; i<n; i++){
-            int curr = int(columnTitle[i]) - int('A') + 1;
+            int curr = columnTitle[i] - 'A' + 1;
             res = res*26 + curr;
         }
         return res;
