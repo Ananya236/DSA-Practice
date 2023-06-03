4a818f3beda861cf6dbdb87ddc13ae3c0db60fe5
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-06-03 18:55:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> v(m,vector<int>(n));
+//         *************using DP**************
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0) v[i][j]=1;
-                else v[i][j] = v[i-1][j] + v[i][j-1];
-            }
+//         vector<vector<int>> v(m,vector<int>(n));
+        
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 || j==0) v[i][j]=1;
+//                 else v[i][j] = v[i-1][j] + v[i][j-1];
+//             }
+//         }
+        
+//         return v[m-1][n-1];
+        
+        
+//         *************using Math************
+//         ans = (m+n-2)! / ((m-1)! (n-1)!)
+        
+        long ans = 1;
+        for(int i=(m+n-2), j=1; i>=max(m,n); i--,j++){
+            ans= (ans*i)/j;
         }
         
-        return v[m-1][n-1];
+        return ans;
     }
 };
