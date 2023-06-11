76936d764659743102b6caacf7b03d29abb9dad0
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-06-11 13:29:25 +0530

    Time: 4 ms (42.81%), Space: 5.9 MB (95.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        else if(n>0) {
+            if(n&1) //n is odd
+                return x * myPow(x*x,(n-1)/2);
+            else
+                return myPow(x*x, n/2);
+        }
+        else {
+            if(n&1) //n is odd
+                return 1/(x * myPow(x*x,-(n+1)/2));
+            else
+                return 1/myPow(x*x, -(n/2));
+        }
+    }
+};
