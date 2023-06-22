ff7b948392b15e494d69e85bbd9b59121b59c73d
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-06-22 08:36:11 +0530

    Time: 168 ms (97.13%), Space: 60.9 MB (68.48%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<vector<int>> adj(n+1);
+        
+//         for(int i=0;i<trust.size();i++){
+//             adj[trust[i][0]].push_back(trust[i][1]);
+//         }
+        
+//         vector<int> indegree(n+1,0);
+//         for(int i=1;i<=n;i++){
+//             for(auto j: adj[i]){
+//                 indegree[j]++;
+//             }
+//         }
+        
+//         for(int i=1;i<=n;i++){
+//             if(indegree[i]==n-1 && adj[i].size()==0) return i;
+//         }
+        
+//         return -1;
+//     }
+    
+    
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>> adj(n+1);
+        vector<int> indegree(n+1,0),outdegree(n+1,0);
         
         for(int i=0;i<trust.size();i++){
-            adj[trust[i][0]].push_back(trust[i][1]);
-        }
-        
-        vector<int> indegree(n+1,0);
-        for(int i=1;i<=n;i++){
-            for(auto j: adj[i]){
-                indegree[j]++;
-            }
+            outdegree[trust[i][0]]++;
+            indegree[trust[i][1]]++;
         }
         
         for(int i=1;i<=n;i++){
-            if(indegree[i]==n-1 && adj[i].size()==0) return i;
+            if(indegree[i]==n-1 && outdegree[i]==0) return i;
         }
         
         return -1;
