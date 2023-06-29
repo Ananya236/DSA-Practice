78bc1080a65f55edb8c6417f7a9186aa7fd0ad26
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-06-29 08:23:03 +0530

    Time: 183 ms (58.26%), Space: 47.3 MB (84.39%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,62 +1,59 @@
-// class Solution {
-// public:
-//     bool foundCycle(vector<vector<int>> graph, vector<int> &vis, int curr, vector<int> &res){
-//         vis[curr] = 1;
-//         for(int i: graph[curr]){
-//             if(vis[i]==1) return false;
-//             if(vis[i]==0) {
-//                 if(!foundCycle(graph, vis, i, res)) return false;
-//             }
-//         }
-//         vis[curr]=2;
-//         res.push_back(curr);
-//         return true;
-//     }
-    
-//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-//         int n = graph.size();
-//         vector<int> vis(n,0);
-//         vector<int> res;
-        
-//         for(int i=0;i<n;i++)
-//             if(vis[i]==0)
-//                 foundCycle(graph, vis, i, res);
-        
-//         sort(res.begin(), res.end());
-        
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>& visited, vector<int>& res, int curr) {
-        visited[curr] = 1;
-        
-        for (int next : graph[curr]) {
-            if (visited[next] == 1 || (!visited[next] && !dfs(graph, visited, res, next))) {
-                return false;
-            }
+    bool foundCycle(vector<vector<int>>& graph, vector<int> &vis, int curr, vector<int> &res){
+        vis[curr] = 1;
+        for(int i: graph[curr]){
+            if(vis[i]==1 || (!vis[i] && !foundCycle(graph, vis, i, res))) return false;
         }
-        
-        visited[curr] = 2;
+        vis[curr]=2;
         res.push_back(curr);
-        
         return true;
     }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> visited(n, 0);
+        vector<int> vis(n,0);
         vector<int> res;
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(graph, visited, res, i);
-            }
-        }
+        for(int i=0;i<n;i++)
+            if(!vis[i])
+                foundCycle(graph, vis, i, res);
         
         sort(res.begin(), res.end());
+        
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     bool dfs(vector<vector<int>>& graph, vector<int>& visited, vector<int>& res, int curr) {
+//         visited[curr] = 1;
+        
+//         for (int next : graph[curr]) {
+//             if (visited[next] == 1 || (!visited[next] && !dfs(graph, visited, res, next))) {
+//                 return false;
+//             }
+//         }
+        
+//         visited[curr] = 2;
+//         res.push_back(curr);
+        
+//         return true;
+//     }
+    
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<int> visited(n, 0);
+//         vector<int> res;
+        
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 dfs(graph, visited, res, i);
+//             }
+//         }
+        
+//         sort(res.begin(), res.end());
+//         return res;
+//     }
+// };
