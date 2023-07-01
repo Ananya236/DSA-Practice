f471a8d289d8eb18714d8e77295f54c19a2aff30
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-01 18:55:52 +0530

    Time: 26 ms (60.87%), Space: 19.2 MB (32.10%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,11 +3,24 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return;
-        vector<int> temp(n,0);
-        int t=0;
+        // vector<int> temp(n,0);
+        // int t=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]) temp[t++]=nums[i];
+        // }
+        // nums=temp;
+        
+        
+        int nz=0;
+        
         for(int i=0;i<n;i++){
-            if(nums[i]) temp[t++]=nums[i];
+            if(nums[i]){
+                if(i==nz) nz++;
+                else{
+                    nums[nz++]=nums[i];
+                    nums[i]=0;
+                }               
+            }
         }
-        nums=temp;
     }
-};
+}; 
