ce39371a897ce6300da51cf36ebfe250e0008794
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-01 19:42:19 +0530

    Time: 9 ms (57.96%), Space: 12.9 MB (79.68%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,20 +3,29 @@ public:
     int maxProfit(vector<int>& prices) {
         int totalProfit = 0;
         int n = prices.size();
-        int minP = prices[0];
-        int currProfit = 0;
+//         int minP = prices[0];
+//         int currProfit = 0;
+        
+//         for(int i=1;i<n;i++){
+//             if(prices[i]<prices[i-1]) {
+//                 minP=prices[i];
+//                 totalProfit+=currProfit;
+//                 currProfit=0;
+//             }
+//             else {
+//                 currProfit = prices[i]-minP;
+//             }
+//         }
+        
+//         return totalProfit+currProfit;
+        
         
         for(int i=1;i<n;i++){
-            if(prices[i]<prices[i-1]) {
-                minP=prices[i];
-                totalProfit+=currProfit;
-                currProfit=0;
-            }
-            else {
-                currProfit = prices[i]-minP;
+            if(prices[i]>prices[i-1]){
+                totalProfit+=prices[i]-prices[i-1];
             }
         }
         
-        return totalProfit+currProfit;
+        return totalProfit;
     }
 };
