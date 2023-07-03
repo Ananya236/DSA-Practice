09cefc9dae76dff7af5033862d7966395502fbfd
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-03 12:48:28 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (11.28%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -9,20 +9,34 @@ public:
         return pre;
     }
     
-    string divide(vector<string>& strs, int st, int ed){
-        if(st==ed)
-            return strs[st];
-        else {
-            int mid = (st+ed)/2;
-            string sl = divide(strs,st,mid);
-            string sr = divide(strs,mid+1,ed);
-            return prefix(sl,sr);
-        }
-    }
+//     string divide(vector<string>& strs, int st, int ed){
+//         if(st==ed)
+//             return strs[st];
+//         else {
+//             int mid = (st+ed)/2;
+//             string sl = divide(strs,st,mid);
+//             string sr = divide(strs,mid+1,ed);
+//             return prefix(sl,sr);
+//         }
+//     }
+    
+//     string longestCommonPrefix(vector<string>& strs) {
+//         int n = strs.size();
+//         if(n==0) return "";
+//         return divide(strs,0,n-1);
+//     }
     
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         if(n==0) return "";
-        return divide(strs,0,n-1);
+        
+        string pre = strs[0];
+        
+        for(int i=1;i<n;i++){
+            pre = prefix(pre, strs[i]);
+            if(pre=="") return pre;
+        }
+        
+        return pre;
     }
 };
