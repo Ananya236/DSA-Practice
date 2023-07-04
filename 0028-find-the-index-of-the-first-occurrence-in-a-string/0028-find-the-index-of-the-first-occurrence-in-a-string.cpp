10e57e439488fd6f06cfc536935afeb32eab466e
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-04 10:09:05 +0530

    Time: 4 ms (39.27%), Space: 6.2 MB (58.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,10 +4,24 @@ public:
         if(haystack == needle) return 0;
         int n = haystack.length();
         int m = needle.length();
-        for(int st=0;st<=n-m;st++){
-            if(haystack.substr(st,m) == needle)
-                return st;
+        // for(int st=0;st<=n-m;st++){
+        //     if(haystack.substr(st,m) == needle)
+        //         return st;
+        // }
+        // return -1;
+        
+        bool flag;
+        for(int i=0;i<n-m+1;i++){
+            flag=true;
+            for(int j=0;j<m;j++){
+                if(haystack[i+j]!=needle[j]){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag) return i;
         }
+        
         return -1;
     }
 };
