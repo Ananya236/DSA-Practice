662a1ffc9ff68c999491e4f3040f354c4b356170
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-15 13:39:24 +0530

    Time: 24 ms (23.47%), Space: 15.4 MB (7.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,6 +2,29 @@ typedef pair<int,int> pi;
 
 class Solution {
 public:
+    //using min heap
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int,int> um;
+    //     int n = nums.size();
+    //     if(n==1) return nums;
+    //     for(int i=0;i<n;i++){
+    //         um[nums[i]]++;
+    //     }
+    //     priority_queue<pi, vector<pi>, greater<pi>> minh;
+    //     for(auto i: um){
+    //         minh.push(make_pair(i.second,i.first));
+    //         if(minh.size()>k) minh.pop();
+    //     }
+    //     vector<int> res;
+    //     while(minh.size()){
+    //         pair<int,int> i = minh.top();
+    //         minh.pop();
+    //         res.push_back(i.second);
+    //     }
+    //     return res;
+    // }
+    
+    //using bucket sort
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> um;
         int n = nums.size();
