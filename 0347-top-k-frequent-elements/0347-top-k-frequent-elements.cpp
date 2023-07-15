f64618a4e04ceffe4324924dddfda0d092310df0
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2023-07-15 13:38:18 +0530

    Time: 17 ms (63.00%), Space: 15.4 MB (7.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -9,17 +9,16 @@ public:
         for(int i=0;i<n;i++){
             um[nums[i]]++;
         }
-        priority_queue<pi, vector<pi>, greater<pi>> minh;
-        for(auto i: um){
-            minh.push(make_pair(i.second,i.first));
-            if(minh.size()>k) minh.pop();
+        vector<vector<int>> bucket(n+1);
+        for(auto it: um){
+            bucket[it.second].push_back(it.first);
         }
         vector<int> res;
-        while(minh.size()){
-            pair<int,int> i = minh.top();
-            minh.pop();
-            res.push_back(i.second);
+        for(int i=n;i>0 && k>0;i--){
+            res.insert(res.end(), bucket[i].begin(), bucket[i].end());
+            k=k-bucket[i].size();
         }
+        
         return res;
     }
 };
