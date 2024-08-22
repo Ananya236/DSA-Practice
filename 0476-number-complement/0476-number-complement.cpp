88e241a7d65332dfeda05184eb2951fd920e2bd4
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2024-08-22 22:27:27 +0530

    Time: 2 ms (50.07%), Space: 7.5 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int findComplement(int num) {
-        if (num == 0) return 1;
+        if (num == 0) return 1; 
+
+        unsigned int bitmask = ~0; 
         
-        unsigned int mask = ~0;
         
-        while (num & mask) {
-            mask <<= 1;
+        while (num & bitmask) {
+            bitmask <<= 1;
         }
-        
-        return ~mask & ~num;
+
+      
+        return num ^ ~bitmask;
     }
 };
