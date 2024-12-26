ff9621a07ea335d2be7a63a97120807e4626e8bc
Ananya236 <88206794+Ananya236@users.noreply.github.com>
2024-12-27 00:03:46 +0530

    Time: 11 ms (16.22%), Space: 18.8 MB (27.92%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -17,28 +17,53 @@ public:
     
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
         
         for(vector<int> pq:  prerequisites){
             graph[pq[1]].push_back(pq[0]);
+            indegree[pq[0]]++;
         }
         
-        vector<int> vis(numCourses, 0);
-        vector<int> path(numCourses, 0);
-        stack<int> s;
-        
+        queue<int> q;
         for(int i=0; i<numCourses; i++){
-            if(!vis[i]) {
-                bool res = dfs(i, graph, vis, s, path);
-                if(res) return {};
-            }
+            if(!indegree[i]) q.push(i);
         }
         
         vector<int> res;
-        while(!s.empty()){
-            res.push_back(s.top());
-            s.pop();
+        while(!q.empty()){
+            int t = q.front();
+            q.pop();
+            res.push_back(t);
+            
+            for(int node: graph[t]){
+                indegree[node]--;
+                if(!indegree[node]) q.push(node);
+            }
         }
         
+        if(res.size() != numCourses) return {};
         return res;
+        
+        //DFS
+        
+        // vector<int> vis(numCourses, 0);
+        // vector<int> path(numCourses, 0);
+        // stack<int> s;
+        
+        // for(int i=0; i<numCourses; i++){
+        //     if(!vis[i]) {
+        //         bool res = dfs(i, graph, vis, s, path);
+        //         if(res) return {};
+        //     }
+        // }
+        
+        // vector<int> res;
+        // while(!s.empty()){
+        //     res.push_back(s.top());
+        //     s.pop();
+        // }
+        
+        // return res;
+        
     }
 };
